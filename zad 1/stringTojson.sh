8ac4af7c06c171bf07366643c964c4c74ea2c550
Miłosz Osiński <mylosz@yahoo.pl>
2013-10-16 15:22:54 +0200

    bardziej przyjazny dla użytkownika

--- zad 1/stringTojson.sh
+++ zad 1/stringTojson.sh
@@ -3,6 +3,15 @@
 # mylosz@yahoo.pl
 ###################
 
-sed "s/ /\"}\n{\"word\":\"/g"  text8 > text8.json
-sed -i '1s/^/{\"word\":\"/' text8.json
-sed -i.bck '$s/$/\"}/' text8.json
+if [ -z "$1" ] ; then
+  echo "Program zamienia wszystkie spację w pliku na }\n{\"word\":\""
+  echo "Na początki pliku dodaje jeszcze {\"word\":\" i na końcu \"}"
+  echo "Użycie $0 input.txt output.json"
+  exit 1;
+fi
+
+sed "s/ /\"}\n{\"word\":\"/g"  > "$2"
+sed -i '1s/^/{\"word\":\"/' "$2"
+sed -i.bck '$s/$/\"}/' "$2"
+
+echo "Gotowe!"
